c4a92c16ad4d257ab72f4ab4dda0f952e106ef9a
irfan-shah-sherani <mrirfanshahsherani@gmail.com>
2025-07-12 19:28:16 +0500

    set downlaod

--- app.js
+++ app.js
@@ -75,7 +75,7 @@ app.get("/Records/For/pdf/:id", (req, res) => {
 
 app.get("/downloadpdf/:id", async (req, res) => {
   const id = req.params.id;
-  const qrcode_url = `http://localhost:3000/openpdf/?id=${id}`;
+  const publicUrl = `http://localhost:3000/openpdf/?id=${id}`;
 
   try {
     const response = await fetch("https://api.pdfshift.io/v3/convert/pdf", {
